6703a33b9cf99489d25d5a68aed54ccbc6aec421
Glynn Bird <glynn.bird@gmail.com>
2026-03-30 10:35:59 +0100

    better error handling

--- frontend/app/pages/v.vue
+++ frontend/app/pages/v.vue
@@ -2,7 +2,13 @@
   const route = useRoute()
   const id = route.query.id
   const {playbackURL, setRecKey} = useRecording()
+  const showAudio = ref(true)
   setRecKey(id)
+
+  function handleAudioError(e) {
+    console.log('handle audio error', e)
+    showAudio.value = false
+  }
 </script>
 <style>
 .note {
@@ -11,11 +17,20 @@
 </style>
 <template>
   <v-alert
+    v-if="showAudio"
     class="note"
     title="Audio Recording"
     text="Note: This recording will be removed after 30 days."
     type="info"
     variant="tonal"
   ></v-alert>
-  <audio preload="auto" controls :src="playbackURL"></audio>
+  <audio v-if="showAudio" @error="handleAudioError" preload="auto" controls :src="playbackURL"></audio>
+  <v-alert
+    v-if="!showAudio"
+    class="note"
+    title="Audio Recording Error"
+    text="Could not play audio recording. Note: The recordings are be removed after 30 days."
+    type="error"
+    variant="tonal"
+  ></v-alert>
 </template>
